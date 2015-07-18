100d019eaf26356decf5fab90b8bf33b2fc67bbf
Jason Gross <jgross@mit.edu>
2015-07-18 16:20:38 -0400

    Use the new docker-based Travis infrastructure
    This allows us to cache the download of packages from ctan, which was a
    step that was occasionally failing.  Now we will fail less, I think.
    
    Followed the instructions at
    http://docs.travis-ci.com/user/migrating-from-legacy/.

--- etc/ci/wget_retry.sh
+++ etc/ci/wget_retry.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+cd "$HOME"/.cache/latex
+
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
     echo "try $i"
-    wget -t 1 "$@" && exit 0
+    wget -N -t 1 "$@" && exit 0
 done
